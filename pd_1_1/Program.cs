ac824b2707302347bbcae4b5b90c16717dcee3d7
LigaKlavina <ligaiklavinai@gmail.com>
2018-05-10 21:22:49 +0300

    3uzdevumi

--- pd_1_1/Program.cs
+++ pd_1_1/Program.cs
@@ -6,7 +6,63 @@ namespace pd_1_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //1
+            Console.WriteLine("ka tevi sauc");
+
+            string ievade = Console.ReadLine();
+
+            Console.WriteLine("sveiki," + ievade);
+
+            //2
+
+            Console.WriteLine("kura gada esi dzimis");
+
+            string dzimsanas_datums = Console.ReadLine();
+
+            int mainiagais;
+
+            mainiagais = Convert.ToInt16(dzimsanas_datums);
+
+            Console.WriteLine("kurs gads ir sogad");
+
+            string gads = Console.ReadLine();
+
+            int mainigais1;
+
+            mainigais1 = Convert.ToInt16(gads);
+
+            Console.WriteLine(mainigais1-mainiagais);
+
+            //3
+            Console.WriteLine("ievadit rinka linijas radiusu");
+
+            string radius = Console.ReadLine();
+
+            int mainigais2;
+
+            mainigais2 = Convert.ToInt16(radius);//radius ir 3.14
+
+            Console.WriteLine();
+
+            int mainigais3;
+
+            mainigais3 = 2* mainigais2;
+
+            double  mainigais4;
+
+            mainigais4 = 2*3.14 * mainigais2;
+
+            Console.WriteLine("diametrs ir " + mainigais3);
+            Console.WriteLine("laukums ir " + mainigais4);
+
+
+
+
+
+
+
+            Console.ReadLine();
         }
+
     }
 }
